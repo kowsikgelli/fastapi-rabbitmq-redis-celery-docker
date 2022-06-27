f8c23c794829e7513cdc05daf4b2d65fb49a9859
kowsikgelli <jaikrishnagelli@gmail.com>
2022-06-27 17:06:46 +0530

    removing static files

--- project/main.py
+++ project/main.py
@@ -1,21 +1,16 @@
 from celery.result import AsyncResult
-from fastapi import Body, FastAPI, Form, Request
+from fastapi import Body, FastAPI
 from fastapi.responses import JSONResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 from worker import create_task
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
 
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("home.html", context={"request": request})
+def home():
+    return "app works"
 
 
 @app.post("/tasks", status_code=201)
